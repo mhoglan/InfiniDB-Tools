e940d9c90f1adc245b99fab0c741ea027cd37078
root <root@ip-172-31-7-18.us-west-2.compute.internal>
2017-11-02 20:02:04 +0000

    Add support for MariaDB XA

--- build_images.sh
+++ build_images.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Execute subshell, && operations so build does not execute if cd fails
-(docker build -t infinidb:base -f ./dockerfiles/infinidb_base/Dockerfile .)
-(docker build -t infinidb:4.6.0-1 ./dockerfiles/infinidb_4.6.0-1)
-(docker build -t infinidb:4.6.3-1 ./dockerfiles/infinidb_4.6.3-1 && docker tag infinidb:4.6.3-1 infinidb:latest)
-(docker build -t infinidb:data ./dockerfiles/infinidb_data)
+(docker build -t mariadb-columnstore:base -f ./dockerfiles/mariadb-columnstore_base/Dockerfile --squash .)
+#(docker build -t infinidb:base -f ./dockerfiles/infinidb_base/Dockerfile .)
+#(docker build -t infinidb:4.6.0-1 ./dockerfiles/infinidb_4.6.0-1)
+#(docker build -t infinidb:4.6.3-1 ./dockerfiles/infinidb_4.6.3-1 && docker tag infinidb:4.6.3-1 infinidb:latest)
+#(docker build -t infinidb:data ./dockerfiles/infinidb_data)
